7c33da87abcc3631bf6c958962d147e72fb60c7b
dotnet-code <@dotnet-code>
2023-05-30 15:12:49 +0530

    commit from code202305301249

---
+++ Dcom/src/app/component/component.component.ts
@@ -0,0 +1,10 @@
+﻿@Component({
+    selector: 'app-component',
+    templateUrl: './component.html'
+})
+export class Component {
+    error: string = "'NoneType' object has no attribute 'replace'";
+
+    constructor(){
+    }
+}
